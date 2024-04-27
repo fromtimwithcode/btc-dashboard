c41df17f54d0e7fff0239671e85d695a28dd3458
Tim Marchant <fromtimwithcode@gmail.com>
2024-04-27 18:16:00 +0700

    shadcn init

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,12 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/app/globals.css";
+import { Inter as FontSans } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata = {
   title: "Bitcoin Dashboard",
@@ -10,8 +15,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
