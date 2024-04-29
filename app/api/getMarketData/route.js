cdaf5af344bff015359c3ae3fef435212de2eab4
Tim Marchant <fromtimwithcode@gmail.com>
2024-04-29 16:34:31 +0700

    pull data from coinmarketcap

---
+++ app/api/getMarketData/route.js
@@ -0,0 +1,15 @@
+export async function GET() {
+  // ID 1 = BTC
+  const res = await fetch(
+    `https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?id=1`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "X-CMC_PRO_API_KEY": process.env.COINMARKETCAP_API_KEY,
+      },
+    },
+  );
+  const marketData = await res.json();
+
+  return Response.json({ marketData });
+}
